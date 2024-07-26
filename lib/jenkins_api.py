3b80bdd6b403b1e60391e1b6c244ae8163469dcc
rafal <ravmiecznik@gmail.com>
2024-07-26 13:35:43 +0200

    username variable fix

--- lib/jenkins_api.py
+++ lib/jenkins_api.py
@@ -11,10 +11,10 @@ def get_jenkins_server(jenkins_base_address: str = 'localhost:8080',
     if not (username and api_token):
         # Loading credentials from the .netrc file.
         netrc_credentials = netrc.netrc()
-        jenkins_username, _, api_token = netrc_credentials.authenticators(jenkins_base_address)
+        username, _, api_token = netrc_credentials.authenticators(jenkins_base_address)
 
     jenkins_full_address = f'{jenkins_base_address}/jenkins'
     jenkins_project_url = f'http://{jenkins_full_address}'
 
-    jenkins_server = jenkins.Jenkins(jenkins_project_url, jenkins_username, api_token)
+    jenkins_server = jenkins.Jenkins(jenkins_project_url, username, api_token)
     return jenkins_server
